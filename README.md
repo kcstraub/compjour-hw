ba2cae328ef6f1bb3237f80c13ce7a3e5e3e4ff2
Katie Straub <kcstraub@dn0a2237bb.sunet>
2015-04-08 16:07:40 -0700

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello there, my name is _Katie_
+
+This is a kitten: 
+
+![img](http://placekitten.com/g/300/300)
---
+++ opener-project/hello-world.py
@@ -0,0 +1,9 @@
+print("Hello world")
+myname = "Katie"
+print "My name is", myname
+myage = 105
+print"I am", myage, "years old"
+print"That is roughly", 105 * 365, "days"
+print"Or,", 105*365*24*60*60, "seconds"
+
+
