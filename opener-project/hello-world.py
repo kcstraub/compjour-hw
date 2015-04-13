7ea1cebb3a8be7e35a20a5c1fdd7c6d2828038fa
Katie Straub <kcstraub@dn0a2233b4.sunet>
2015-04-13 11:56:47 -0700

    hello

--- opener-project/hello-world.py
+++ opener-project/hello-world.py
@@ -1,9 +1,9 @@
 print("Hello world")
 myname = "Katie"
-print "My name is", myname
+print("My name is", myname)
 myage = 105
-print"I am", myage, "years old"
-print"That is roughly", 105 * 365, "days"
-print"Or,", 105*365*24*60*60, "seconds"
+print("I am", myage, "years old")
+print("That is roughly", 105 * 365, "days")
+print("Or,", 105*365*24*60*60, "seconds")
 
 
